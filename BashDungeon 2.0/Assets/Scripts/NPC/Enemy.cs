d49e793c7495dcf9c0d5846028e3273e3fbc2945
Marco1693 <giallo022@live.it>
2019-01-18 12:41:51 +0100

    Enemy script

---
+++ BashDungeon 2.0/Assets/Scripts/NPC/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    Animator animator;
+    public Room currentRoom;
+    GameObject gameManager;
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        animator = this.GetComponent<Animator>();
+        gameManager = GameObject.Find("GameManager");
+        currentRoom = gameManager.GetComponent<LevelGeneration>().GetRoomByName(this.gameObject.GetComponentInParent<Transform>().name);
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Fly();
+	}
+
+    public void Fly()
+    {
+        if (currentRoom == player.GetComponent<PlayerMovement>().currentRoom)
+        {
+            animator.SetBool("PlayerIsHere", true);
+        }
+        else
+        {
+            animator.SetBool("PlayerIsHere", false);
+        }
+    }
+}
