3d36d11fc828feecbba78545f8bf7fd39eae5672
Marco1693 <giallo022@live.it>
2019-01-22 11:13:02 +0100

    health bar text e hp

--- BashDungeon 2.0/Assets/Scripts/Player/HealthBar.cs
+++ BashDungeon 2.0/Assets/Scripts/Player/HealthBar.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour {
 
@@ -14,6 +15,7 @@ public class HealthBar : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.localScale = new Vector3((health / 100f), transform.localScale.y, transform.localScale.z);
+        GameObject.Find("HP").GetComponent<Text>().text = ("HP ") + health + ("/100");
     }
 
     public void LoseHealth(float hp)
