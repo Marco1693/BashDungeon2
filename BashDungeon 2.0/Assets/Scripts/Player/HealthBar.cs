d6776749a7bce014dee0d837baa556e828fa732f
Marco1693 <giallo022@live.it>
2019-01-22 12:55:51 +0100

    addHealth

--- BashDungeon 2.0/Assets/Scripts/Player/HealthBar.cs
+++ BashDungeon 2.0/Assets/Scripts/Player/HealthBar.cs
@@ -7,10 +7,11 @@ using UnityEngine.SceneManagement;
 public class HealthBar : MonoBehaviour {
 
     public float health = 100;
+    float maxHealth = 100;
 
 	void Update () {
 
-        transform.localScale = new Vector3((health / 100f), transform.localScale.y, transform.localScale.z);
+        transform.localScale = new Vector3((health / maxHealth), transform.localScale.y, transform.localScale.z);
         GameObject.Find("HP").GetComponent<Text>().text = ("HP ") + health + ("/100");
         Die();
     }
@@ -25,6 +26,18 @@ public class HealthBar : MonoBehaviour {
         {
             health = 0;
         }        
+    }
+
+    public void HealthPotion(float hp)
+    {
+        if((health + hp) <= maxHealth)
+        {
+            health += hp;
+        }
+        else
+        {
+            health = maxHealth;
+        }
     }
 
     public void Die()
